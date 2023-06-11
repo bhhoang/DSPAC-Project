1e0b593782321fad9ee89456f63c61fde55a6770
R1verrrr <116377003+R1verrrr@users.noreply.github.com>
2023-06-11 22:59:45 +0700

    Update gauss_filter.py

--- Gaussian Filter/gauss_filter.py
+++ Gaussian Filter/gauss_filter.py
@@ -2,27 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import scipy.signal as signal
-from. import dataset
+from . import dataset
 
-def gkernel(l, sig):
-    """\
-    Gaussian Kernel Creator via given length and sigma
-    """
+def gaussian_kernel(size, sigma):
+    kernel = np.fromfunction(lambda x, y: (1/(2*np.pi*sigma**2)) * np.exp(-((x-(size-1)/2)**2 + (y-(size-1)/2)**2)/(2*sigma**2)), (size, size))
+    kernel /= np.sum(kernel)
+    return kernel
 
-    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
-    xx, yy = np.meshgrid(ax, ax)
+def convolve(image, kernel):
+    # Get kernel size
+    ksize = kernel.shape[0]
 
-    kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(sig))
+    # Get image dimensions
+    height, width = image.shape
 
-    return kernel / np.sum(kernel)
+    # Calculate padding size
+    pad = ksize // 2
 
-img = cv2.imread('JFK.png', 0)
-# img = img.astype(np.float32) / 255.0
-k = gkernel(9, 2)
-denoise_img = signal.convolve2d(img, k, mode='same', boundary='symm')
+    # Create padded image
+    padded_image = np.pad(image, ((pad, pad), (pad, pad)), mode='edge')
 
-plt.imshow(img, cmap='gray')
-plt.show()
+    # Initialize output image
+    output = np.zeros_like(image)
 
-plt.imshow(denoise_img, cmap='gray')
+    # Convolution
+    for i in range(height):
+        for j in range(width):
+            output[i, j] = np.sum(padded_image[i:i+ksize, j:j+ksize] * kernel)
+
+    return output
+
+# Example usage:
+# Load the image
+image = cv2.imread('JFK.png', cv2.IMREAD_GRAYSCALE)
+
+# Convert the image to float
+image = image.astype(float)
+
+# Normalize the image to the range [0, 1]
+image /= 255.0
+
+# Define the kernel size and sigma
+kernel_size = 7
+sigma = 2.0
+
+# Generate the Gaussian kernel
+kernel = gaussian_kernel(kernel_size, sigma)
+
+# Apply convolution with the Gaussian kernel
+filtered_image = convolve(image, kernel)
+
+# Display the original and filtered images
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(filtered_image, cmap='gray')
+plt.title('Filtered Image (Gaussian)')
+plt.axis('off')
+
+plt.tight_layout()
 plt.show()
