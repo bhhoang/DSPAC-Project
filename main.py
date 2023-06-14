157ba18ef866dddded4fd7259bdd1e5e2adb6d5c
Kontact <duyan5745@gmail.com>
2023-06-14 19:41:11 +0700

    feat: add periodic noise filter demo

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import cv2
 from matplotlib import pyplot as plt
+from scipy.fft import fft2, fftshift, ifft2, ifftshift
+import numpy as np
+
 from utils.addnoise import periodic_noise
 from utils.rgb2gray import rgb2gray
 import Filters.fft_denoiser, Filters.gauss_filter, Filters.metrics, Filters.NLM, Filters.median_filter
 from Filters.NLM import NLMeans
+from Filters.notch_filter import notch_reject_filter
 
 
 def image_load(path):
@@ -42,6 +47,50 @@ def plotter(img1, img2, title) -> None:
     plt.show()
 
 
+def periodic_noise_demo(img: np.ndarray) -> None:
+    # Convert
+
+    # Apply periodic noise
+    image = img.copy()
+    img_periodic = periodic_noise(image)
+
+    # Get the magnitude spectrum
+    ft = fft2(img_periodic)
+    fs = fftshift(ft)
+    fft_mag = 20 * np.log(np.abs(fs))
+
+    # Create notch reject filters
+    H1 = notch_reject_filter(fs.shape, 15, 176, 176)
+    H2 = notch_reject_filter(fs.shape, 15, 0, 350 - 176)
+    H3 = notch_reject_filter(fs.shape, 15, 350 - 176, 0)
+    H4 = notch_reject_filter(fs.shape, 15, -350 + 176, 176)
+
+    # Apply the filters to the image fft
+    H = H1 * H2 * H3 * H4
+    filtered_ft_mag = fft_mag * H
+
+    # Show the original image and fft
+    fig = plt.figure(figsize=(20, 20))
+    fig.add_subplot(2, 2, 1)
+    plt.imshow(img_periodic, cmap='gray')
+    plt.title('Original Image')
+
+    fig.add_subplot(2, 2, 2)
+    plt.imshow(fft_mag, cmap='gray')
+    plt.title('Original FT')
+
+    # Show the filtered image and filtered fft
+    fig.add_subplot(2, 2, 3)
+    plt.imshow(ifft2(ifftshift(fs * H)).real, cmap='gray')
+    plt.title('Filtered Image')
+
+    fig.add_subplot(2, 2, 4)
+    plt.imshow(filtered_ft_mag, cmap='gray')
+    plt.title('Filtered FT')
+
+    plt.show()
+
+
 def main() -> None:
     # Load image
     img = image_load('./dataset/JFK.png')
@@ -78,6 +127,10 @@ def main() -> None:
 
     # Plot all filters
     multiple_plot(img, img_gauss, median_filter_image, gauss_noise, fft_filter_image)
+
+    # Image with periodic noise
+    img2 = cv2.imread('./dataset/bw_landscape.jpeg', 0)
+    periodic_noise_demo(img2)
 
 
 if __name__ == '__main__':
